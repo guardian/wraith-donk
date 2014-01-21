d1455690cc7325785f78f085ef070cb44948ec4c
meza <meza@meza.hu>
2014-01-21 19:23:34 +0000

    initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'nokogiri'
+gem 'wraith', "1.1.0", :git => "git://github.com/meza/wraith"
---
+++ wraithDaemon.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'wraith/manager'
+
+#set :root, File.dirname(__FILE__)
+#set :static, true
+#set :public_dir, Proc.new { File.join(File.dirname(__FILE__), "public") }
+
+
+get '/' do
+
+  pid = fork do
+    @wraith_manager = WraithManager.new('config');
+    @wraith_manager.reset_shots_folder
+    @wraith_manager.check_for_paths
+    @wraith_manager.save_images
+    @wraith_manager.crop_images
+    @wraith_manager.compare_images
+    @wraith_manager.generate_thumbnails
+    @wraith_manager.generate_gallery
+  end
+
+  "Started process pid: #{pid}"
+
+end
