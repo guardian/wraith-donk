925431598c76e1798856b455ebf4d25dedef9def
meza <meza@meza.hu>
2014-01-30 14:57:59 +0000

    daemon config

--- wraithDaemon.rb
+++ wraithDaemon.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
 require 'wraith'
+require 'net/smtp'
+require 'yaml'
+
+@daemonConfig = YAML::load(File.open("configs/config.yaml"))
 
 def doit(pidFile)
+
+
   File.open(pidFile, 'w') { |file| file.write("") }
   @config = ('config')
   folders = Wraith::FolderManager.new(@config)
@@ -20,12 +26,33 @@ def doit(pidFile)
   gallery = Wraith::GalleryGenerator.new(@save_images.directory)
   gallery.generate_gallery
   File.delete pidFile
+  email
+
+end
+
+def email
+
+
+  message = <<MESSAGE
+From: #{@daemonConfig['wrait_daemon']['notifications']['from']}
+To: #{@daemonConfig['wrait_daemon']['notifications']['to']}
+Subject: #{@daemonConfig['wrait_daemon']['notifications']['subject']}
+
+wraith done with errors
+MESSAGE
+
+  Net::SMTP.start('mx.gc2.dc1.gnm') do |smtp|
+    smtp.send_message message, @daemonConfig['wrait_daemon']['notifications']['from'], @daemonConfig['wrait_daemon']['notifications']['to']
+  end
+
+
 end
 
 #set :root, File.dirname(__FILE__)
 #set :static, true
 #set :public_dir, Proc.new { File.join(File.dirname(__FILE__), "public") }
 
+set :port, @daemonConfig['wrait_daemon']['port']
 
 get '/' do
 
@@ -37,16 +64,16 @@ get '/' do
     return "Work already in progress, check the gallery for results"
   end
 
-  if params.include? "fork"
-    pid = fork do
-      doit(pidFile)
-    end
-    File.open(pidFile, 'w') { |file| file.write("#{pid}") }
-    return "Started process pid: #{pid}<br/>The results will be visible at /gallery.html"
+  #if params.include? "fork"
+  pid = fork do
+    doit(pidFile)
   end
-
-  doit(pidFile)
-
-  redirect to '/gallery.html'
+  File.open(pidFile, 'w') { |file| file.write("#{pid}") }
+  "Started process pid: #{pid}<br/>The results will be visible at /gallery.html"
+  #end
+  #
+  #doit(pidFile)
+  #
+  #redirect to '/gallery.html'
 
 end
