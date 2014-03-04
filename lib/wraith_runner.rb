40fb620149cf47222fbf1aef158b1aa8148c335e
DaveBlooman <david.blooman@gmail.com>
2014-03-04 21:21:40 +0000

    tidy

--- lib/wraith_runner.rb
+++ lib/wraith_runner.rb
@@ -1,12 +1,9 @@
 class WraithRunner
+  attr_reader :directory
 
   def initialize(config_name)
     @config = (config_name)
     @directory = "/public/#{config_name}"
-  end
-
-  def directory
-    @directory
   end
 
   def run_wraith
@@ -14,14 +11,14 @@ class WraithRunner
     start.capture(@config)
   end
 
-  def has_differences
+  def has_differences?
     Dir.glob("#{directory}/*/*.txt") do |fn|
       data = File.open(fn, 'rb') { |io| io.read }
       @diff = 0 + data.to_i
     end
 
     unless @diff.is_a?(Numeric)
-	    return true
+      return true
     end
     @diff > 0
   end
